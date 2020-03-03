8e51d704064405de98ab76a46ebd8f89759fae8e
LSO107 <34274877+LSO107@users.noreply.github.com>
2020-03-03 14:55:21 +0000

    Basic Player Health
    Damage/Heal
    Health Regeneration

---
+++ Roguelite/Assets/Scripts/Player/Health/HealthObject.cs
@@ -0,0 +1,57 @@
+﻿using UI.Player.Health;
+using UnityEngine;
+
+namespace Player.Health
+{
+    [RequireComponent(typeof(HealthBarUpdater))]
+    internal sealed class HealthObject : MonoBehaviour
+    {
+        public HealthDefinition healthDefinition;
+
+        private float m_NextRegenerationTime;
+        private float m_RegenerationTime = 10;
+
+        public void Start()
+        {
+            healthDefinition = new HealthDefinition();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                healthDefinition.Damage();
+            }
+
+            RegenerateHealth();
+        }
+
+        /// <summary>
+        /// Regenerate the health over time
+        /// </summary>
+        private void RegenerateHealth()
+        {
+            // TODO: Prevent regeneration during combat
+
+            if (Time.time >= m_NextRegenerationTime)
+            {
+                healthDefinition.Heal(1);
+                m_NextRegenerationTime = Time.time + m_RegenerationTime;
+            }
+        }
+
+        private void HandleDamage()
+        {
+            healthDefinition.Damage();
+
+            if (healthDefinition.IsDead)
+            {
+                // Play Death Animation
+
+                // Remove Items & Equipment
+
+                // Reset Player (Health and Location)
+            }
+        }
+    }
+}
