810a7f83611bd57d1cf1ec2dabe3d15ca2511f44
LSO107 <34274877+LSO107@users.noreply.github.com>
2020-03-04 17:11:47 +0000

    Equipment UI art
    Character Page Art, no functionality.

---
+++ Roguelite/Assets/Scripts/Extensions/CanvasGroupExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+    public static class CanvasGroupExtensions
+    {
+        public static void ToggleCanvasGroup(this CanvasGroup group, bool show)
+        {
+            group.alpha = show ? 1 : 0;
+            group.blocksRaycasts = show;
+            group.interactable = show;
+        }
+    }
+}
