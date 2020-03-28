bf22c344adbca15477d8c2eb92322d86c9b3b8c2
LSO107 <34274877+LSO107@users.noreply.github.com>
2020-03-28 17:06:09 +0000

    Day Night Cycle

---
+++ Roguelite/Assets/Scripts/TorchManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+internal sealed class TorchManager : MonoBehaviour
+{
+    [SerializeField] private DayNightCycle dayNightCycle;
+
+    private ParticleSystem[] m_Torches;
+
+    private void Start()
+    {
+        m_Torches = GetComponentsInChildren<ParticleSystem>();
+        dayNightCycle.RegisterScheduledEvent(0.25f, () => ToggleLights(false));
+        dayNightCycle.RegisterScheduledEvent(0.75f, () => ToggleLights(true));
+    }
+
+    private void ToggleLights(bool on)
+    {
+        foreach (var torch in m_Torches)
+        {
+            if (on)
+            {
+                torch.Play();
+            }
+            else
+            {
+                torch.Stop();
+            }
+        }
+    }
+}
