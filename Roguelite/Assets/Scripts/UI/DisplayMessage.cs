f9883e8742b8d7b25722dba54a3d07875de74d72
LSO107 <34274877+LSO107@users.noreply.github.com>
2020-05-20 11:53:00 +0100

    Display Message
    Currently used on unavailable shops i.e. magic and cosmetic.

---
+++ Roguelite/Assets/Scripts/UI/DisplayMessage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    internal sealed class DisplayMessage : MonoBehaviour
+    {
+        [SerializeField] private InteractNotice m_InteractNotice;
+        [SerializeField] private string m_Message;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            m_InteractNotice.Show(m_Message);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            m_InteractNotice.Hide();
+        }
+    }
+}
